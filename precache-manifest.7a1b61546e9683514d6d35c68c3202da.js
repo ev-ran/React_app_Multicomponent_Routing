8c0daf3b79dc5a0f772b64ede1d6bac707cbe6a3
Evgenia Rangaev <alraner93@gmail.com>
2020-08-28 15:39:16 -0400

    Updates

---
+++ precache-manifest.7a1b61546e9683514d6d35c68c3202da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44a5ae64143d029e29bfe9ee5eb3c2d",
+    "url": "/React_app_Multicomponent_Routing/index.html"
+  },
+  {
+    "revision": "8a06372df9d0f579956f",
+    "url": "/React_app_Multicomponent_Routing/static/css/main.553d5387.chunk.css"
+  },
+  {
+    "revision": "f98b2e5e56d8221b00f8",
+    "url": "/React_app_Multicomponent_Routing/static/js/2.1603782d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_app_Multicomponent_Routing/static/js/2.1603782d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a06372df9d0f579956f",
+    "url": "/React_app_Multicomponent_Routing/static/js/main.1509b161.chunk.js"
+  },
+  {
+    "revision": "7b28318c453af0b264d4",
+    "url": "/React_app_Multicomponent_Routing/static/js/runtime-main.6c28279b.js"
+  },
+  {
+    "revision": "0fa69445c4d37a9d86d0f72f21e8a221",
+    "url": "/React_app_Multicomponent_Routing/static/media/nothingToSee.0fa69445.png"
+  }
+]);
